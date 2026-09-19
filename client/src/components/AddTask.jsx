45d0e5c237aec2ddefc7dfa9d70524de1390b01c
EC2 Default User <ec2-user@ip-172-31-4-70.ec2.internal>
2026-09-19 21:57:06 +0000

    ajuste nos arquivos dockerfile, AddTask.jsx

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar Task dominio com HTTPS 
+        Adicionar Task dominio CND com cloudfront 
       </button>
       
       <Modal
