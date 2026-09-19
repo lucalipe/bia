0447dc343ff52f303e299d779437135bd112bbd4
EC2 Default User <ec2-user@ip-172-31-4-70.ec2.internal>
2026-09-19 02:26:37 +0000

    Alteração do arquivo client/src/components/AddTask.jsx

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar Task com CI-CD 
+        Adicionar Task dominio com HTTPS 
       </button>
       
       <Modal
